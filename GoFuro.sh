c359407ff731a0b2b63a02ed177cceb2a46ec3ab
Ma Eikei <maeikei@gmail.com>
2016-01-04 16:28:57 +0900

    cp files from sd card to ramdisk

--- GoFuro.sh
+++ GoFuro.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 date
 rm -rf /dev/shm/Dropbox-Uploader/DOButtonGoFuro
+if [ -d /dev/shm/Dropbox-Uploader ]
+then
+  echo "have been checkout form github do noting"
+else
+  cp -rf /home/pi/GoFuro/Dropbox-Uploader /dev/shm/
+fi
+
+
 cd /dev/shm/Dropbox-Uploader/ && ./dropbox_uploader.sh download DOButtonGoFuro
 
 if [ -f /dev/shm/Dropbox-Uploader/DOButtonGoFuro/gofuro.txt ] 
